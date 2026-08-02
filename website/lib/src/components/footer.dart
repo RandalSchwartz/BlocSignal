3b04830b5a668aed4a9abf44886111cd88585584
Randal L. Schwartz <merlyn@stonehenge.com>
2026-08-01 17:57:21 -0700

    docs(website): update footer license to MIT License

--- website/lib/src/components/footer.dart
+++ website/lib/src/components/footer.dart
@@ -9,7 +9,7 @@ class Footer extends StatelessComponent {
     return footer(classes: 'site-footer', [
       div(classes: 'container footer-content', [
         p([
-          Component.text('Released under the BSD 3-Clause License. Managed by Randal L. Schwartz & Open-Source Contributors.'),
+          Component.text('Released under the MIT License. Managed by Randal L. Schwartz & Open-Source Contributors.'),
         ]),
         p(classes: 'footer-links', [
           a(href: 'https://pub.dev/packages/bloc_signals', target: Target.blank, [Component.text('pub.dev')]),
